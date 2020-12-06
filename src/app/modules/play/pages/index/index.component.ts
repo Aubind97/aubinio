a5fcc260a280fd6fff540e80a5cade9f8e218020
Aubind97 <aubind97@orange.fr>
2020-12-06 15:47:12 +0100

    Add basic speed control

--- src/app/modules/play/pages/index/index.component.ts
+++ src/app/modules/play/pages/index/index.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { Hand } from 'src/app/core/models/global';
+import { CoreMusicService } from 'src/app/core/services/core-music.service';
 import { PlayService } from './../../services/play.service';
 
 @Component({
@@ -11,7 +12,10 @@ import { PlayService } from './../../services/play.service';
 export class IndexComponent implements OnInit, OnDestroy {
   selectedHands$ = this.playService.selectedHands$;
 
-  constructor(private playService: PlayService) {}
+  speedAvailables = [0.1, 0.25, 0.33, 0.5, 0.75, 0.88, 1, 1.25, 1.5];
+  currentSpeedIndex = 6;
+
+  constructor(private playService: PlayService, private coreMusicService: CoreMusicService) {}
 
   ngOnDestroy(): void {
     this.playService.stopWaiting();
@@ -23,5 +27,10 @@ export class IndexComponent implements OnInit, OnDestroy {
 
   toggleSelectedHand(hand: Hand): void {
     this.playService.toogleSelectedHand(hand);
+  }
+
+  changeSpeed() {
+    this.currentSpeedIndex = (this.currentSpeedIndex + 1) % this.speedAvailables.length;
+    this.coreMusicService.setSpeed(this.speedAvailables[this.currentSpeedIndex]);
   }
 }
